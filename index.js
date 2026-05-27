d62331b0faf09f593fa013bd14eac6369c2b4033
thedevaman <devaman2508@gmail.com>
2026-05-27 09:03:31 +0530

    fetch and update done in mongodb

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {MongoClient}= require("mongodb")
+const {MongoClient,ObjectId}= require("mongodb")
 const app = express()
 let db = null
 app.listen(8080,()=>console.log("server is running"))
@@ -12,6 +12,7 @@ mongo.connect()
 
 .then((conn)=>{
     db = conn.db("coading_ott")
+    console.log("Database Connected")
 })
 
 
@@ -22,4 +23,22 @@ app.post("/users",async(req,res)=>{
     const userCollection = db.collection("users")
     await userCollection.insertOne(req.body)
     res.status(200).json({message:"User Created"})
+})
+
+app.get("/users-list",async(req,res)=>{
+
+    const userCollection = db.collection("users")
+   const users = await userCollection.find().toArray()
+    res.status(200).json({message:"User list",users})
+})
+
+app.put("/users-update/:id",async(req,res)=>{
+    const id = new ObjectId(req.params.id)
+    const body = req.body
+
+    const userCollection = db.collection("users")
+    const updatedusers = await userCollection.updateOne({_id:id},{$set:body})
+
+     res.status(200).json(updatedusers)
+
 })
