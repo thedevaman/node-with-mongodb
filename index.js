a58cee6d07298a9892d64b0d621842453a9db2ff
thedevaman <devaman2508@gmail.com>
2026-05-27 08:43:02 +0530

    data storein momgo db

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const {MongoClient}= require("mongodb")
 const app = express()
+let db = null
 app.listen(8080,()=>console.log("server is running"))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 const mongo = new MongoClient("mongodb://localhost:27017")
 mongo.connect()
 
-.then(()=>console.log("Database Connected"))
+.then((conn)=>{
+    db = conn.db("coading_ott")
+})
 
 
 .catch(()=>console.log("Database not Connected"))
+
+app.post("/users",async(req,res)=>{
+
+    const userCollection = db.collection("users")
+    await userCollection.insertOne(req.body)
+    res.status(200).json({message:"User Created"})
+})
